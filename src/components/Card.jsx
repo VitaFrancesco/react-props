2a49d168ba6e9cc8bf6d178300bcd70489058c82
VitaFrancesco <vitafrancesco00@gmail.com>
2024-11-28 17:23:54 +0100

    ADD: tags to card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -8,9 +8,22 @@ export default function Card(props) {
             </div>
             <div className={style.cardBody}>
                 <h2>{props.title}</h2>
-                {/* <div className={style.tags}>
-                    {props.tags.map((tag) => )}
-                </div> */}
+                <div className={style.tags}>
+                    {props.tags.map((tag) => (tag === 'html' ?
+                        <div className={style.html}>{tag}</div> :
+                        (tag === 'css' ?
+                            <div className={style.css}>{tag}</div> : (
+                                tag === 'js' ?
+                                    <div className={style.js}>{tag}</div> : (
+                                        tag === 'php' ?
+                                            <div className={style.php}>{tag}</div> :
+                                            <div>{tag}</div>
+                                    )
+                            )
+
+                        )
+                    ))}
+                </div>
                 <p>{props.content}</p>
                 <button>Leggi di più</button>
             </div>
